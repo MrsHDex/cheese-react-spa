d6f7901be824e1008f7dfbf277a0a084b68155e6
MrsHDex <thompsonac542@gmail.com>
2019-05-10 14:51:28 -0400

    Initial Setup of HomeView

---
+++ src/views/HomeView.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Col from "react-bootstrap/Col";
+
+
+class HomeView extends Component {
+
+    render() {
+        return(
+            <h5>Welcome to your personal cheese repository!</h5>
+             
+        );
+      }
+    }
+
+
+
+
+export default HomeView;
